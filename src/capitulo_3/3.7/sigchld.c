52a7d9fdd2cee75a05fc1bc7b8c219a9c68f30fe
Mario Gueyraud <mgueyraud.junior@gmail.com>
2021-09-04 12:07:47 -0400

    Se agrega el fork

--- src/capitulo_3/3.7/sigchld.c
+++ src/capitulo_3/3.7/sigchld.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+
 sig_atomic_t child_exit_status;
 
 void clean_up_child_process (int signal_number)
@@ -20,7 +23,21 @@ int main ()
     memset (&sigchld_action, 0, sizeof (sigchld_action));
     sigchld_action.sa_handler = &clean_up_child_process;
     sigaction (SIGCHLD, &sigchld_action, NULL);
+
     /* Now do things, including forking a child process. */
+
+    pid_t child_pid;
+    printf ("the main program process ID is %d\n", (int) getpid());
+    child_pid = fork () ;
+    if (child_pid != 0) {
+        printf ("this is the parent process, with id %d\n", (int) getpid ());
+        printf ("the child's process ID is %d\n",(int) child_pid );
+    }
+    else
+        printf ("this is the child process, with id %d\n", (int) getpid ());
+
+
     /* ... */
+
     return 0;
 }
